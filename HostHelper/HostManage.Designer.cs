eb3922761f8bea554d8fa3282c157e5075f1a238
EDISON\edison.zuo <edison.zuo@bestwehotel.com>
2018-04-17 11:29:36 +0800

    初始化导入项目

---
+++ HostHelper/HostManage.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace HostHelper
+{
+    partial class HostManage
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HostManage));
+            this.notifyIconHost = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStripHost = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ToolStripMenuItemManage = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItemAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItemExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancle = new System.Windows.Forms.Button();
+            this.richTextBoxHost = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStripHost.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIconHost
+            // 
+            this.notifyIconHost.ContextMenuStrip = this.contextMenuStripHost;
+            this.notifyIconHost.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconHost.Icon")));
+            this.notifyIconHost.Text = "Host助手";
+            this.notifyIconHost.Visible = true;
+            // 
+            // contextMenuStripHost
+            // 
+            this.contextMenuStripHost.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.contextMenuStripHost.BackgroundImage = global::HostHelper.Properties.Resources.bg;
+            this.contextMenuStripHost.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.contextMenuStripHost.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItemManage,
+            this.ToolStripMenuItemAbout,
+            this.ToolStripMenuItemExit});
+            this.contextMenuStripHost.Name = "contextMenuStripHost";
+            this.contextMenuStripHost.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.contextMenuStripHost.Size = new System.Drawing.Size(153, 92);
+            // 
+            // ToolStripMenuItemManage
+            // 
+            this.ToolStripMenuItemManage.MergeIndex = 2;
+            this.ToolStripMenuItemManage.Name = "ToolStripMenuItemManage";
+            this.ToolStripMenuItemManage.Size = new System.Drawing.Size(152, 22);
+            this.ToolStripMenuItemManage.Text = "HOST管理";
+            this.ToolStripMenuItemManage.Click += new System.EventHandler(this.ToolStripMenuItemManage_Click);
+            // 
+            // ToolStripMenuItemAbout
+            // 
+            this.ToolStripMenuItemAbout.MergeIndex = 1;
+            this.ToolStripMenuItemAbout.Name = "ToolStripMenuItemAbout";
+            this.ToolStripMenuItemAbout.Size = new System.Drawing.Size(152, 22);
+            this.ToolStripMenuItemAbout.Text = "关于";
+            this.ToolStripMenuItemAbout.Click += new System.EventHandler(this.ToolStripMenuItemAbout_Click);
+            // 
+            // ToolStripMenuItemExit
+            // 
+            this.ToolStripMenuItemExit.MergeIndex = 0;
+            this.ToolStripMenuItemExit.Name = "ToolStripMenuItemExit";
+            this.ToolStripMenuItemExit.Size = new System.Drawing.Size(152, 22);
+            this.ToolStripMenuItemExit.Text = "退出";
+            this.ToolStripMenuItemExit.Click += new System.EventHandler(this.ToolStripMenuItemExit_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Location = new System.Drawing.Point(95, 370);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancle
+            // 
+            this.btnCancle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnCancle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancle.Location = new System.Drawing.Point(191, 370);
+            this.btnCancle.Name = "btnCancle";
+            this.btnCancle.Size = new System.Drawing.Size(75, 23);
+            this.btnCancle.TabIndex = 4;
+            this.btnCancle.Text = "取消";
+            this.btnCancle.UseVisualStyleBackColor = true;
+            this.btnCancle.Click += new System.EventHandler(this.btnCancle_Click);
+            // 
+            // richTextBoxHost
+            // 
+            this.richTextBoxHost.Location = new System.Drawing.Point(-1, 0);
+            this.richTextBoxHost.Name = "richTextBoxHost";
+            this.richTextBoxHost.Size = new System.Drawing.Size(387, 355);
+            this.richTextBoxHost.TabIndex = 5;
+            this.richTextBoxHost.Text = "";
+            // 
+            // HostManage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 405);
+            this.Controls.Add(this.richTextBoxHost);
+            this.Controls.Add(this.btnCancle);
+            this.Controls.Add(this.btnSave);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "HostManage";
+            this.Opacity = 0D;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Host助手";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.HostManage_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStripHost.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIconHost;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripHost;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemExit;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemAbout;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemManage;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancle;
+        private System.Windows.Forms.RichTextBox richTextBoxHost;
+    }
+}
+
---
+++ HostHelper/HostManage.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using Microsoft.Win32;
+
+namespace HostHelper
+{
+    public partial class HostManage : Form
+    {
+        public HostManage()
+        {
+            InitializeComponent();
+        }
+        /**程序退出标识
+         * */
+        private static Boolean exit = false;
+        private Hashtable  groupDir ;
+        private ArrayList settingInfoList ;
+        private ToolStripMenuItem toolStripMenuItemDiy;
+        private ToolStripMenuItem toolStripMenuItemDiyChild;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(!IsAutoRun("HostHelper")){
+                SetAutoRun("HostHelper", System.Environment.CurrentDirectory + "\\HostHelper.exe");
+            }
+            //this.Left = (int)((Screen.PrimaryScreen.Bounds.Width - this.Width) / 2);
+            //this.Top = (int)((Screen.PrimaryScreen.Bounds.Height - this.Height) / 2);  
+
+            if (setToolStripItem())
+            {
+                this.Visible = false;
+                this.Opacity = 0;
+            }
+            else {
+                MessageBox.Show("配置文件有错，请检查～！","温馨提示");
+                this.Visible = true;
+                this.Opacity = 100;
+                readHostHelper();
+            }
+
+          
+        }
+
+        #region 设置host 菜单
+        private bool setToolStripItem(){
+
+            bool result=true;
+            try
+            {
+                groupDir = getHostSettingInfo();
+
+                foreach (DictionaryEntry group in groupDir)
+                {
+                    //检查菜单中是否存在
+                    if (!hasToolStripMenuItem(group.Key.ToString()))
+                    {
+                        //设置组名
+                        toolStripMenuItemDiy = new System.Windows.Forms.ToolStripMenuItem();
+                        toolStripMenuItemDiy.Name = "ToolStripMenuItemHost";
+                        toolStripMenuItemDiy.Size = new System.Drawing.Size(118, 22);
+                        toolStripMenuItemDiy.Text = group.Key.ToString();
+                        toolStripMenuItemDiy.Tag = group.Key.ToString();
+                        toolStripMenuItemDiy.MergeIndex = 2;
+                        toolStripMenuItemDiy.Click += new System.EventHandler(this.ToolStripMenuItemSetGroupHost_Click);
+                        //添加到组
+                        foreach (String settingInfo in (ArrayList)group.Value)
+                        {
+                            toolStripMenuItemDiyChild = new System.Windows.Forms.ToolStripMenuItem();
+                            toolStripMenuItemDiyChild.Name = "ToolStripMenuItemHost";
+                            toolStripMenuItemDiyChild.Size = new System.Drawing.Size(118, 22);
+                            toolStripMenuItemDiyChild.Text = settingInfo.Split(' ')[1];
+                            toolStripMenuItemDiyChild.Tag = settingInfo.Split(' ')[0];
+                            toolStripMenuItemDiyChild.MergeIndex = 2;
+                            toolStripMenuItemDiyChild.Click += new System.EventHandler(this.ToolStripMenuItemSetChildHost_Click);
+                            toolStripMenuItemDiy.DropDownItems.Insert(toolStripMenuItemDiy.DropDownItems.Count, toolStripMenuItemDiyChild);
+                        }
+                        this.contextMenuStripHost.Items.Insert(contextMenuStripHost.Items.Count - 3, toolStripMenuItemDiy);
+                    }
+                }
+            }catch(Exception ex){
+                result = false;
+            }
+            return result;
+        }
+        #endregion
+
+        private bool hasToolStripMenuItem(String key) {
+            bool result = false;
+            foreach (ToolStripMenuItem item in this.contextMenuStripHost.Items)
+            {
+               if(item.Text.Equals(key)){
+                   result = true;
+                   break;
+               }
+           }
+            return result;
+        }
+
+        private bool hasToolStripDownMenuItem(String key)
+        {
+            bool result = false;
+            foreach (ToolStripMenuItem item in this.contextMenuStripHost.Items)
+            {
+                if (item.Text.Equals(key))
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        #region 程序退出
+        private void ToolStripMenuItemExit_Click(object sender, EventArgs e)
+        {
+            exit = true;
+            Application.Exit();
+        }
+        #endregion
+        #region 关于我们
+        private void ToolStripMenuItemAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("程序员百味制作  交流网站：http://www.bywei.cn", "关于我们", MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+        #endregion
+
+        #region Host管理
+        private void ToolStripMenuItemManage_Click(object sender, EventArgs e)
+        {
+            readHostHelper();
+        }
+        #endregion
+
+        #region 读取HostHelper.inc
+        private void readHostHelper() {
+            this.WindowState = System.Windows.Forms.FormWindowState.Normal;
+            if (!this.Visible)
+            {
+                this.Visible = true;
+            }
+            if (this.Opacity == 0)
+            {
+                this.Opacity = 100;
+            }
+            if (File.Exists("HostHelper.inc"))
+            {
+                this.richTextBoxHost.Text = "";
+                FileStream hostSettingInfoStream = new FileStream("HostHelper.inc", FileMode.Open, FileAccess.Read);
+                StreamReader reader = new StreamReader(hostSettingInfoStream);
+                String readLine = "";
+                while ((readLine = reader.ReadLine()) != null && !"".Equals(readLine))
+                {
+                    this.richTextBoxHost.Text += readLine.Trim() + "\n";
+                }
+                reader.Close();
+                hostSettingInfoStream.Close();
+            }
+            else {
+                MessageBox.Show("缺少配置文件 HostHelper.inc ","温馨提示");
+            }
+        }
+        #endregion
+
+        #region 保存host
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            FileStream hostSettingInfoWriteStream = new FileStream("HostHelper.inc", FileMode.Truncate, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(hostSettingInfoWriteStream);
+            foreach (String settingInfo in this.richTextBoxHost.Text.Split('\n'))
+            {
+                if (!"".Equals(settingInfo))
+                {
+                    writer.WriteLine(settingInfo);
+                }
+            }
+            writer.Flush();
+            writer.Close();
+            hostSettingInfoWriteStream.Close();
+            this.Visible = false;
+
+            setToolStripItem();
+        }
+        #endregion
+
+        #region 获取配置信息
+        private Hashtable getHostSettingInfo() {
+            groupDir = new Hashtable();
+            settingInfoList = new ArrayList();
+            FileStream hostSettingInfoStream = new FileStream("HostHelper.inc", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(hostSettingInfoStream);
+            int indexOfOne=0;
+            int indexOfTwo = 0;
+            String groupName="";
+            String readLine = "";
+            while ((readLine = reader.ReadLine()) != null && !"".Equals(readLine))
+            {
+                indexOfOne=readLine.IndexOf("#");
+                indexOfTwo = readLine.IndexOf("##");
+                if(indexOfOne == 0 && indexOfTwo ==-1){
+                   groupName=readLine.Substring(1,readLine.Length-1);
+                }
+                else if (indexOfTwo!=0)
+                {
+                   settingInfoList.Add(readLine);
+                }
+                if (indexOfTwo == 0)
+                {
+                    groupDir.Add(groupName, settingInfoList);
+                    settingInfoList = new ArrayList();
+                }
+            }
+            reader.Close();
+            hostSettingInfoStream.Close();
+            return groupDir;
+        }
+        #endregion
+
+
+       private void ToolStripMenuItemSetChildHost_Click(object sender, EventArgs e){
+           String hostDomain = ((ToolStripItem)sender).Text;
+           String hostIp = ((ToolStripItem)sender).Tag.ToString();
+           String filePath=@"C:\WINDOWS\system32\drivers\etc\hosts";
+           if(File.Exists(filePath)){
+               FileStream hostSettingInfoWriteStream = new FileStream(filePath, FileMode.Truncate, FileAccess.Write);
+               StreamWriter writer = new StreamWriter(hostSettingInfoWriteStream);
+               writer.WriteLine(hostIp+" "+hostDomain);
+               writer.Flush();
+               writer.Close();
+               hostSettingInfoWriteStream.Close();
+           }else{
+             MessageBox.Show("hosts文件不存在","温馨提示");
+           }
+           this.contextMenuStripHost.Hide();
+        }
+
+       private void ToolStripMenuItemSetGroupHost_Click(object sender, EventArgs e)
+       {
+            String hostGroup=((ToolStripItem)sender).Text;
+            String filePath=@"C:\WINDOWS\system32\drivers\etc\hosts";
+            if (File.Exists(filePath))
+            {
+                FileStream hostSettingInfoWriteStream = new FileStream(filePath, FileMode.Truncate, FileAccess.Write);
+                StreamWriter writer = new StreamWriter(hostSettingInfoWriteStream);
+                foreach (String settingInfo in (ArrayList)groupDir[hostGroup])
+                {
+                    writer.WriteLine(settingInfo.Split(' ')[0] + " " + settingInfo.Split(' ')[1]);
+                }
+                writer.Flush();
+                writer.Close();
+                hostSettingInfoWriteStream.Close();
+            }else
+            {
+                MessageBox.Show("hosts文件不存在", "温馨提示");
+            }
+            this.contextMenuStripHost.Hide();
+       }
+
+        private void HostManage_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!exit)
+            {
+                e.Cancel = true;
+                this.Visible = false;
+            }
+        }
+
+        private void btnCancle_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+        }
+        #region 开机启动
+        public static bool SetAutoRun(string keyName, string filePath)
+    {
+        try
+        {
+            RegistryKey hkml = Registry.LocalMachine;
+            RegistryKey runKey = hkml.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+            runKey.SetValue(keyName, filePath);
+            runKey.Close();
+        }
+        catch
+        {
+            return false;
+        }
+        return true;
+    }
+    /// <summary>
+    /// 判断是否已经设置为开机启动
+    /// </summary>
+    /// <param name="keyName">注册表key</param>
+    /// 楼雨：2010-12-1 16:43
+    public bool IsAutoRun(string keyName)
+    {
+        try
+        {
+            bool _exit = false;
+            RegistryKey hkml = Registry.LocalMachine;
+            RegistryKey runKey = hkml.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+ 
+            string[] subkeyNames;
+            subkeyNames = runKey.GetValueNames();
+            foreach (string kName in subkeyNames)
+            {
+                if (kName == keyName)
+                {
+                    _exit = true;
+                    return _exit;
+                }
+            }
+            return _exit;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    #endregion
+
+    }
+
+
+}
